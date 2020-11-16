13b65c011f168839b798d24c9521bfb6e37f69dc
bashangbasir <38761135+bashangbasir@users.noreply.github.com>
2020-11-16 20:57:43 +0800

    Add find element method.

--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -1,8 +1,12 @@
 package base;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
 
 public class BaseTests {
 
@@ -12,13 +16,21 @@ public class BaseTests {
         System.setProperty("webdriver.chrome.driver", "resources/chromedriver.exe");
         driver = new ChromeDriver();
         driver.get("https://the-internet.herokuapp.com/");
-
         //fullscreen the browser
         driver.manage().window().maximize();
         //same as above
-        driver.manage().window().fullscreen();
+        //driver.manage().window().fullscreen();
         //set custom size
-        driver.manage().window().setSize(new Dimension(375,812));
+        //driver.manage().window().setSize(new Dimension(375,812));
+
+        //find the elements
+        List<WebElement> links = driver.findElements(By.tagName("a"));
+        System.out.println(links.size());
+
+        //find the element
+        WebElement inputsLink = driver.findElement(By.linkText("Inputs"));
+        //click the element
+        inputsLink.click();
 
         System.out.println(driver.getTitle());
         driver.quit();
