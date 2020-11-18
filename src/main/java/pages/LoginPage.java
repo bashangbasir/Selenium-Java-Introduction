d79af390ab4f557134337f4134023bc1a39b2124
bashangbasir <38761135+bashangbasir@users.noreply.github.com>
2020-11-18 23:27:16 +0800

    Refactor for more readability.

--- src/main/java/pages/LoginPage.java
+++ src/main/java/pages/LoginPage.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class LoginPage {
+
     private WebDriver driver;
     private By usernameElem = By.id("username");
     private By passwordElem = By.id("password");
