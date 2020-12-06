6d61cf7f7f49119d14f27ce1a424ba3c91a74160
bashangbasir <38761135+bashangbasir@users.noreply.github.com>
2020-12-06 23:33:30 +0800

    Add DynamicLoadExample1page, DynamicLoadExample2Page class.

---
+++ src/main/java/pages/DynamicLoadExample1Page.java
@@ -0,0 +1,52 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class DynamicLoadExample1Page {
+
+    private WebDriver driver;
+    private By startBtn = By.cssSelector("#start button");
+    private By loadingIndicator = By.id("loading");
+    private By loadedText = By.id("finish");
+
+    public DynamicLoadExample1Page(WebDriver driver){
+        this.driver = driver;
+    }
+
+    public void clickStartButton(){
+        driver.findElement(startBtn).click();
+
+        WebDriverWait wait = new WebDriverWait(driver, 5);
+        wait.until(ExpectedConditions.invisibilityOf(
+                driver.findElement(loadingIndicator)));
+    }
+
+    public void clickStartButtonFluentWait(){
+        driver.findElement(startBtn).click();
+
+        FluentWait wait = new FluentWait(driver)
+                .withTimeout(Duration.ofSeconds(5))
+                .pollingEvery(Duration.ofSeconds(1))
+                .ignoring(NoSuchElementException.class);
+
+        wait.until(ExpectedConditions.invisibilityOf(
+                driver.findElement(loadingIndicator)));
+
+    }
+
+
+    public String getLoadedText(){
+        return driver.findElement(loadedText).getText();
+    }
+
+
+
+
+}
---
+++ src/main/java/pages/DynamicLoadExample2Page.java
@@ -0,0 +1,43 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class DynamicLoadExample2Page {
+
+    private WebDriver driver;
+    private By startBtn = By.cssSelector("#start button");
+    private By loadingIndicator = By.id("loading");
+    private By loadedText = By.id("finish");
+
+    public DynamicLoadExample2Page(WebDriver driver){
+        this.driver = driver;
+    }
+
+    public void clickStartButton(){
+        driver.findElement(startBtn).click();
+        WebDriverWait wait = new WebDriverWait(driver,5);
+        wait.until(ExpectedConditions.presenceOfElementLocated(loadedText));
+    }
+
+    public void clickStartButtonFluentWait(){
+        driver.findElement(startBtn).click();
+
+        FluentWait wait = new FluentWait(driver)
+                .withTimeout(Duration.ofSeconds(5))
+                .pollingEvery(Duration.ofSeconds(1))
+                .ignoring(NoSuchElementException.class);
+        wait.until(ExpectedConditions.presenceOfElementLocated(loadedText));
+
+    }
+
+    public String getLoadedText() {
+        return driver.findElement(loadedText).getText();
+    }
+}
