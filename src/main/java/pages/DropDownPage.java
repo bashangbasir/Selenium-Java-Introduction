0e22ffbfea2b620b70730c67316532a2b83e721b
bashangbasir <38761135+bashangbasir@users.noreply.github.com>
2020-12-09 00:22:08 +0800

    Add method for changing the dropdown to be able to select multiple option.

--- src/main/java/pages/DropDownPage.java
+++ src/main/java/pages/DropDownPage.java
@@ -1,6 +1,7 @@
 package pages;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
@@ -11,7 +12,7 @@ import java.util.stream.Collectors;
 public class DropDownPage {
 
     private WebDriver driver;
-    private By dropDown_elem = By.id("dropdown");
+    private By dropdown = By.id("dropdown");
 
     public DropDownPage(WebDriver driver){
         this.driver = driver;
@@ -26,7 +27,13 @@ public class DropDownPage {
         return selectedElems.stream().map(e->e.getText()).collect(Collectors.toList());
     }
 
+    public void addMultipleAttribute(){
+        String script = "arguments[0].setAttribute('multiple','')";
+        var jsExecutor = (JavascriptExecutor)driver;
+        jsExecutor.executeScript(script,findDropDownElement());
+    }
+
     private Select findDropDownElement(){
-        return new Select(driver.findElement(dropDown_elem));
+        return new Select(driver.findElement(dropdown));
     }
 }
